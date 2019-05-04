08dd0b3bac50512f7488657721a81d78f6bfb7ff
cptwalmart <cptwalmart@gmail.com>
2019-05-04 09:56:38 -0400

    upload of notes, makefile, and main.cpp

---
+++ main.cpp
@@ -0,0 +1,3 @@
+/* Kevin Koch COSC 320 Final Project
+/* Topic: Graph Isomorphism
+/* Goals: To demonstrate some brute-force Graph Isomorphism testing on different algorithms and explore the complexities of a NP-HARD problem */
